b7eefedc7365ebeba0afef1c0a55e0b4f1d26977
andrei.andronic <andrei.andronic@gmail.com>
2015-04-09 11:43:18 -0400

    Adding Scenario 1 Speclow
    Adding SpecFlow for the first scenario

---
+++ dotnet/SpecFlowTests/CheckOut.feature
@@ -0,0 +1,13 @@
+﻿Feature: CheckOut
+	In order to avoid silly mistakes
+	As a math idiot
+	I want to be told the sum of two numbers
+
+@mytag
+Scenario: The user and the system confirms the order
+	Given That there are items in the shopping cart
+	When The User confirms the order
+	And The User inputs the billing information
+	And The System confirms the order
+	Then The System updates the inventory
+	And The System displays order confirmation
---
+++ dotnet/SpecFlowTests/CheckOutSteps.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using TechTalk.SpecFlow;
+
+namespace SpecFlowTests
+{
+    [Binding]
+    public class CheckOutSteps
+    {
+        [Given(@"That there are items in the shopping cart")]
+public void GivenThatThereAreItemsInTheShoppingCart()
+{
+    ScenarioContext.Current.Pending();
+}
+
+        [When(@"The User confirms the order")]
+public void WhenTheUserConfirmsTheOrder()
+{
+    ScenarioContext.Current.Pending();
+}
+
+        [When(@"The User inputs the billing information")]
+public void WhenTheUserInputsTheBillingInformation()
+{
+    ScenarioContext.Current.Pending();
+}
+
+        [When(@"The System confirms the order")]
+public void WhenTheSystemConfirmsTheOrder()
+{
+    ScenarioContext.Current.Pending();
+}
+
+        [Then(@"The System updates the inventory")]
+public void ThenTheSystemUpdatesTheInventory()
+{
+    ScenarioContext.Current.Pending();
+}
+
+        [Then(@"The System displays order confirmation")]
+public void ThenTheSystemDisplaysOrderConfirmation()
+{
+    ScenarioContext.Current.Pending();
+}
+    }
+}
